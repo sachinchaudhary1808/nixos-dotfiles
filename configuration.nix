9ffeea7eb98cd85b48f42c2a0e8df5494873667c
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-11-06 14:07:55 +0530

    moved cachix setup settings from nixConfig to nix.settings

--- configuration.nix
+++ configuration.nix
@@ -12,6 +12,7 @@
   nix.package = pkgs.unstable.lix;
   nix.nixPath = [ "nixpkgs=${inputs.nixpkgs}" ];
 
+  # for cachix builds
   nix.settings.extra-substituters = [ "https://nix-community.cachix.org" ];
   nix.settings.extra-trusted-public-keys = [
     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
