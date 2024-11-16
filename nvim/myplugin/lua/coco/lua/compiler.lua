58dd4de9da355f454a4cff34f52a7a6c632bfb58
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2024-11-16 14:19:31 +0530

    up:compiler.lua
    added more functionality

--- nvim/myplugin/lua/coco/lua/compiler.lua
+++ nvim/myplugin/lua/coco/lua/compiler.lua
@@ -3,78 +3,108 @@ vim.fn.mkdir("/tmp/bin", "p")
 
 -- Function to generate a unique filename based on the current timestamp
 local function generate_unique_name()
-	return vim.fn.strftime("%Y%m%d%H%M%S")
+    return vim.fn.strftime("%Y%m%d%H%M%S")
 end
 
--- Define commands for each filetype
-local compile_commands = {
-	c = function(unique_name)
-		return "gcc % -o " .. unique_name .. " -lm"
-	end,
-	cpp = function(unique_name)
-		return "g++ % -o " .. unique_name .. " -lm"
-	end,
-	python = function()
-		return "python3 %"
-	end,
-	javascript = function()
-		return "node %"
-	end,
-	go = function()
-		-- return "go run % "
-		return 'go run "' .. vim.fn.expand("%:p:h") .. '"'
-	end,
-	lua = function()
-		return "lua %"
-	end,
-	-- Add more languages as needed
+-- Define commands for each filetype with their compilation needs
+local language_configs = {
+    c = {
+        needs_compilation = true,
+        get_command = function(unique_name)
+            return {
+                compile = "gcc % -o " .. unique_name .. " -lm",
+                run = unique_name,
+            }
+        end,
+    },
+    cpp = {
+        needs_compilation = true,
+        get_command = function(unique_name)
+            return {
+                compile = "g++ % -o " .. unique_name .. " -lm",
+                run = unique_name,
+            }
+        end,
+    },
+    python = {
+        needs_compilation = false,
+        get_command = function()
+            return {
+                run = "python3 %",
+            }
+        end,
+    },
+    javascript = {
+        needs_compilation = false,
+        get_command = function()
+            return {
+                run = "node %",
+            }
+        end,
+    },
+    go = {
+        needs_compilation = false,
+        get_command = function()
+            return {
+                run = 'go run "' .. vim.fn.expand("%:p:h") .. '"',
+            }
+        end,
+    },
+    lua = {
+        needs_compilation = false,
+        get_command = function()
+            return {
+                run = "lua %",
+            }
+        end,
+    },
 }
 
 -- Function to compile and run the program
 local function compile_and_run()
-	-- Save the current file
-	vim.cmd("w")
+    -- Save the current file
+    vim.cmd("w")
 
-	-- Get the filetype of the current buffer
-	local filetype = vim.bo.filetype
+    -- Get the filetype of the current buffer
+    local filetype = vim.bo.filetype
 
-	-- Generate a unique output name in /tmp/bin
-	local unique_name = "/tmp/bin/" .. vim.fn.expand("%:t:r") .. "_" .. generate_unique_name()
+    -- Check if we have a configuration for this filetype
+    local config = language_configs[filetype]
+    if not config then
+        vim.notify("No configuration defined for filetype: " .. filetype, vim.log.levels.ERROR)
+        return
+    end
 
-	-- Get the appropriate compile command for the current filetype
-	local compile_cmd
-	if compile_commands[filetype] then
-		compile_cmd = compile_commands[filetype](unique_name)
-	else
-		vim.notify("No compile command defined for filetype: " .. filetype, vim.log.levels.ERROR)
-		return
-	end
+    -- Generate a unique output name in /tmp/bin for compiled languages
+    local unique_name
+    if config.needs_compilation then
+        unique_name = "/tmp/bin/" .. vim.fn.expand("%:t:r") .. "_" .. generate_unique_name()
+    end
 
-	-- Set the makeprg to use the command
-	vim.o.makeprg = compile_cmd
-	vim.o.shellpipe = ">%s 2>&1"
+    -- Get the commands for this filetype
+    local commands = config.get_command(unique_name)
 
-	-- Run makeprg (compilation or execution)
-	vim.cmd("make")
+    if config.needs_compilation then
+        -- For compiled languages
+        vim.o.makeprg = commands.compile
+        vim.o.shellpipe = ">%s 2>&1"
 
-	-- Check if there was an error
-	if vim.v.shell_error == 0 then
-		-- Close the quickfix window if it is open
-		vim.cmd("cclose")
+        -- Compile the program
+        vim.cmd("silent make")
 
-		-- For compiled languages (C, C++, Go), run the binary in a terminal
-		if filetype == "c" or filetype == "cpp" then
-			vim.cmd("belowright split | terminal " .. unique_name)
-		else
-			-- For interpreted languages (Python, JavaScript), just run the script
-			vim.cmd("belowright split | terminal " .. vim.o.makeprg)
-		end
-	else
-		-- Open the quickfix window to display compilation errors
-		vim.cmd("copen")
-		-- Map Enter to close the quickfix window
-		-- vim.api.nvim_buf_set_keymap(0, "n", "<CR>", ":cclose<CR>", { noremap = true, silent = true })
-	end
+        -- Check if compilation was successful
+        if vim.v.shell_error == 0 then
+            vim.cmd("cclose")
+            -- Run the compiled binary in a terminal
+            vim.cmd("belowright split | terminal " .. commands.run)
+        else
+            -- Show compilation errors
+            vim.cmd("copen")
+        end
+    else
+        -- For interpreted languages, directly run in terminal
+        vim.cmd("belowright split | terminal " .. commands.run)
+    end
 end
 
 -- Keybind to compile and run the program in a new terminal window
