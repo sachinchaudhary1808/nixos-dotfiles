8f1372f566c23c85a193dfd7879e83a57809dfaf
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-01-30 15:20:26 +0530

    enable ssh

--- configuration.nix
+++ configuration.nix
@@ -401,7 +401,7 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = false;
+  services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
