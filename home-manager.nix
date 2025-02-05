7d1d54aef1cb741c77583f05d8c5dc3b9a27781c
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-02-05 19:33:28 +0530

    added rambox

--- home-manager.nix
+++ home-manager.nix
@@ -26,6 +26,7 @@ in {
 
     #Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
     packages = with pkgs; [
+      rambox
       obsidian
       brave
       prismlauncher
