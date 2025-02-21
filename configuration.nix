b05b701bec8d17902ae8e506745df81a467e00e5
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-02-22 03:02:48 +0530

    added emacs

--- configuration.nix
+++ configuration.nix
@@ -243,6 +243,7 @@
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # some good nix tools
+    emacs
     river
     kdiskmark
     deadnix
