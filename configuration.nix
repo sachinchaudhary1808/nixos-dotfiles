cd4610653655e546c113bd73e1efd1e36ff0c804
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-06-07 22:15:07 +0530

    added z-editor and nixd setup for it and bluetooth issue note

--- configuration.nix
+++ configuration.nix
@@ -246,6 +246,7 @@
   environment.systemPackages = with pkgs; [
     # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     # some good nix tools
+    nixd
     gnome-calculator
     river
     kdiskmark
--- notes.md
+++ notes.md
@@ -1,2 +1,4 @@
 - try to use git command-line instead of lazygit
 - use blink cmp when it gets stable version 1.1.1 atleast in nixpkgs
+
+fix bluetooh blueman issues not supporting x11
