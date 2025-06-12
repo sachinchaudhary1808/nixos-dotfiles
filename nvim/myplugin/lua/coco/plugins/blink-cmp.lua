0ea0ee4dff8454b1cc59bc15ed89bf9e27c0f1d8
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-06-12 23:29:02 +0530

    for test

--- nvim/myplugin/lua/coco/plugins/blink-cmp.lua
+++ nvim/myplugin/lua/coco/plugins/blink-cmp.lua
@@ -5,7 +5,7 @@ local winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:Pmen
 
 blink.setup({
     snippets = {
-        preset = "luasnip",
+        -- preset = "luasnip",
         active = function()
             if luasnip.in_snippet() and not blink.is_visible() then
                 return true
@@ -35,39 +35,41 @@ blink.setup({
             winhighlight = winhighlight,
             border = "single",
             draw = {
-                components = {
-                    kind_icon = {
-                        text = function(ctx)
-                            local icon = ctx.kind_icon
-                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                                if dev_icon then
-                                    icon = dev_icon
-                                end
-                            else
-                                icon = require("lspkind").symbolic(ctx.kind, {
-                                    mode = "symbol",
-                                })
-                            end
-
-                            return icon .. ctx.icon_gap
-                        end,
-
-                        -- Optionally, use the highlight groups from nvim-web-devicons
-                        -- You can also add the same function for `kind.highlight` if you want to
-                        -- keep the highlight groups in sync with the icons.
-                        highlight = function(ctx)
-                            local hl = ctx.kind_hl
-                            if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                                if dev_icon then
-                                    hl = dev_hl
-                                end
-                            end
-                            return hl
-                        end,
-                    },
-                },
+                treesitter = { "lsp" },
+                columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
+                -- components = {
+                --     kind_icon = {
+                --         text = function(ctx)
+                --             local icon = ctx.kind_icon
+                --             if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                --                 local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+                --                 if dev_icon then
+                --                     icon = dev_icon
+                --                 end
+                --             else
+                --                 icon = require("lspkind").symbolic(ctx.kind, {
+                --                     mode = "symbol",
+                --                 })
+                --             end
+                --
+                --             return icon .. ctx.icon_gap
+                --         end,
+                --
+                --         -- Optionally, use the highlight groups from nvim-web-devicons
+                --         -- You can also add the same function for `kind.highlight` if you want to
+                --         -- keep the highlight groups in sync with the icons.
+                --         highlight = function(ctx)
+                --             local hl = ctx.kind_hl
+                --             if vim.tbl_contains({ "Path" }, ctx.source_name) then
+                --                 local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+                --                 if dev_icon then
+                --                     hl = dev_hl
+                --                 end
+                --             end
+                --             return hl
+                --         end,
+                --     },
+                -- },
             },
         },
         ghost_text = { enabled = true },
