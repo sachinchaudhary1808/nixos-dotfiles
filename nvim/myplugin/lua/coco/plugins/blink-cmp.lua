9be6ca3b04a16e02a70f07d6507b38726bee8465
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-07-03 14:19:13 +0530

    blink cmp kyempa better making

--- nvim/myplugin/lua/coco/plugins/blink-cmp.lua
+++ nvim/myplugin/lua/coco/plugins/blink-cmp.lua
@@ -97,16 +97,21 @@ blink.setup({
         },
     },
     keymap = {
-        ["<C-Space>"] = { "show", "show_documentation", "hide_documentation" },
-        ["<C-e>"] = { "hide", "fallback" },
-        ["<CR>"] = { "accept", "fallback" },
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
-        ["<Up>"] = { "snippet_forward", "fallback" },
-        ["<Down>"] = { "snippet_backward", "fallback" },
-        ["<C-k>"] = { "select_prev", "fallback" },
-        ["<C-j>"] = { "select_next", "fallback" },
+        ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+        ["<C-e>"] = { "hide" },
+        ["<C-y>"] = { "select_and_accept" },
+
+        ["<Up>"] = { "select_prev", "fallback" },
+        ["<Down>"] = { "select_next", "fallback" },
+        ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
+        ["<C-n>"] = { "select_next", "fallback_to_mappings" },
+
         ["<C-b>"] = { "scroll_documentation_up", "fallback" },
         ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+
+        ["<Tab>"] = { "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "snippet_backward", "fallback" },
+
+        ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
     },
 })
