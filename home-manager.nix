ad5b1ffb6385f6d26390acd4a6a0b6d475df8170
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-07-05 13:08:47 +0530

    added vscode-fhf

--- home-manager.nix
+++ home-manager.nix
@@ -24,6 +24,7 @@ in
 
     #Here goes the rest of your home-manager config, e.g. home.packages = [ pkgs.foo ];
     packages = with pkgs; [
+      vscode-fhs
       geogebra6
       woomer
       blender
