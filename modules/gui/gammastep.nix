daddd7c884b999e2b8d61ed6c4226bf8a27bcf2a
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-09-04 03:36:56 +0530

    tamprature stuff

--- modules/gui/gammastep.nix
+++ modules/gui/gammastep.nix
@@ -1,8 +1,14 @@
 {
   services.gammastep = {
     enable = true;
+    temperature.day = 5000;
+    temperature.night = 2500;
     provider = "manual";
-    settings = {general = {adjustment-method = "wayland";};};
+    settings = {
+      general = {
+        adjustment-method = "wayland";
+      };
+    };
     latitude = "23.03";
     longitude = "72.52";
   };
