c4e6e4d4c81824c430b7bc263fdaf04df153309e
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-09-05 03:41:25 +0530

    updated

--- configuration.nix
+++ configuration.nix
@@ -413,10 +413,12 @@
       };
     };
     extraPortals = with pkgs; [
-      xdg-desktop-portal-gtk
       xdg-desktop-portal-gnome
+      xdg-desktop-portal-gtk
     ];
   };
+
+  programs.dconf.enable = true;
 
   #fonts
   fonts.enableDefaultPackages = true;
