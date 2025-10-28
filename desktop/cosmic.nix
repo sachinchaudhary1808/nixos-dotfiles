5c4692e22bdbe6c45aaf271168b3fbeba3fb8187
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-10-28 12:20:48 +0530

    udpated

--- desktop/cosmic.nix
+++ desktop/cosmic.nix
@@ -1,9 +1,11 @@
-{ config, pkgs }: {
+{ pkgs, ... }: {
   # Enable the COSMIC login manager
   services.displayManager.cosmic-greeter.enable = true;
 
   # Enable the COSMIC desktop environment
   services.desktopManager.cosmic.enable = true;
+
+  services.desktopManager.cosmic.xwayland.enable = true;
 
   # excludePackages
   environment.cosmic.excludePackages = with pkgs; [ cosmic-edit ];
