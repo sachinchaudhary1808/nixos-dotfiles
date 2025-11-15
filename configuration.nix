47c061f1bc3d07aba0468ec89adee54439b4e539
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2025-11-15 18:06:48 +0530

    added plymouth theme and fixed font error by having default fonts

--- configuration.nix
+++ configuration.nix
@@ -43,8 +43,37 @@
   #   };
   # };
 
+  boot = {
+    plymouth = {
+      enable = true;
+      theme = "owl";
+      themePackages = with pkgs;
+        [
+          # By default we would install all themes
+          (adi1090x-plymouth-themes.override { selected_themes = [ "owl" ]; })
+        ];
+    };
+
+    # Enable "Silent boot"
+    consoleLogLevel = 3;
+    initrd.verbose = false;
+    kernelParams = [
+      "quiet"
+      "splash"
+      "boot.shell_on_fail"
+      "udev.log_priority=3"
+      "rd.systemd.show_status=auto"
+    ];
+    # Hide the OS choice for bootloaders.
+    # It's still possible to open the bootloader list by pressing any key
+    # It will just not appear on screen unless a key is pressed
+    loader.timeout = 0;
+  };
+
   boot.loader = {
     systemd-boot.enable = true;
+    systemd-boot.consoleMode = "auto";
+
     grub = {
       enable = false;
       device = "nodev";
@@ -128,8 +157,8 @@
 
   console = {
     earlySetup = true;
-    font = "ter-powerline-v24b";
-    packages = with pkgs; [ terminus_font powerline-fonts ];
+    # font = "ter-powerline-v24b";
+    # packages = with pkgs; [ terminus_font powerline-fonts ];
     keyMap = "us";
   };
 
