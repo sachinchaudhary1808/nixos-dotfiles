9a96232c9d50e16bbeb68e89bb98d05776c90f07
sachinchaudhary1808 <chaudharysachinasachin@gmail.com>
2026-06-26 19:46:30 +0530

    added kitty and updated

--- configuration.nix
+++ configuration.nix
@@ -644,4 +644,8 @@
 
   programs.nix-index-database.comma.enable = true;
 
+  # services.desktopManager.plasma6.enable = true;
+  # services.displayManager.sddm.enable = true;
+  # services.displayManager.sddm.wayland.enable = true;
+
 }
